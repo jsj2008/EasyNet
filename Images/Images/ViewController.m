409eb09200f09e6ba462a46b1e767a91ac1ec499
wangjufan <wangjufan@126.com>
2017-05-12 20:49:45 +0800

    fist

--- Images/Images/ViewController.m
+++ Images/Images/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "EasyImage.h"
+
+
+
+@interface ViewController (){
+    NSMutableArray * urls;
+    NSMutableArray * paras;
+    
+    EasyCacheManager * manager;
+}
 
 @end
 
@@ -17,6 +26,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    paras = [[NSMutableArray alloc] init];
+    urls = [[NSMutableArray alloc] init];
+    
+    manager = [EasyCacheManager new];
+    [urls addObject:@"http://img.ipc.me/uploads/post/17031/thumb/140x100.jpg"];
+//    [urls addObject:@"http://evathumber.avnpc.com/thumb/d/02,c_fill,d_picasa,h_128,w_260.jpg"];
+//    [urls addObject:@"http://evathumber.avnpc.com/thumb/d/05,c_fill,d_picasa,h_128,w_160.jpg"];
+//    [urls addObject:@"http://evathumber.avnpc.com/thumb/d/05,c_fill,d_picasa,h_128,w_160.jpg"];
+//    [urls addObject:@"http://evathumber.avnpc.com/thumb/d/05,c_fill,d_picasa,h_128,w_160.jpg"];
+//    [urls addObject:@"http://evathumber.avnpc.com/thumb/d/01,c_fill,d_flickr,h_270,w_360.jpg"];
+//    [urls addObject:@"http://evathumber.avnpc.com/thumb/d/02,c_fill,d_picasa,h_128,w_260.jpg"];
+    [urls addObject:@"http://img.ipc.me/uploads/post/17031/thumb/140x100.jpg"];
+
+    
+    __weak typeof(self) wself = self;
+    
+    for (NSString * key in urls) {
+        
+        EasyImageParas * para = [manager createEasyImageParas];
+        para.url = key;
+        para.cacher = [manager getEasyDiskCache];
+        para.downloader = [manager getTinyFileDownloader];
+        para.imageView = wself.imageView;
+        para.autoCancel = YES;
+        
+        para.recycleBlock = ^(EasyImageParas * par){
+            [manager collectEasyImageParas:par];
+        };
+        
+        [paras addObject:para];
+        [self.imageView easyImageWithPara:para];
+    }
+    
+    [self.imageView easyGifInMainbundleForName:@"stream_loading"];
+
+//    id<EasyCacheProtocol> cacher = [[EasyDiskCache alloc] init];
+//    NSLog(@"%@", [cacher filePathForKey:@"wjf"]);
+//    NSLog(@"%@", [cacher filePathForKey:@"wjdf"]);
+//    NSLog(@"%@", [cacher filePathForKey:@"wjddf"]);
 }
 
 

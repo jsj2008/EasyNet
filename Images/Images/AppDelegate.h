51bec0671cffed6bc6e75cd08abd041a4f678025
wangjufan <wangjufan@126.com>
2017-05-12 11:18:38 +0800

    images

---
+++ Images/Images/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Images
+//
+//  Created by wangjufan on 12/5/2017.
+//  Copyright © 2017 DuduWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Images/Images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Images
+//
+//  Created by wangjufan on 12/5/2017.
+//  Copyright © 2017 DuduWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
